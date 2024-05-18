bf263cfd353d551c6575713d05e5e672cdef93ae
Daniel Moreira <167491111+danielzmoreira@users.noreply.github.com>
2024-05-17 21:46:02 -0300

    atualizando total

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,10 @@
 function menuShow() {
-   let menuMobile = document.querySelector('.mobile-menu');
+   let menuMobile = document.querySelector('.mobile-menu-icon');
    if (menuMobile.classlist.contains('open')) {
-    menuMobile.classList.remove('open');}
-    else {
-        menuMobile.classList.add('open');}
+    menuMobile.classList.remove('open');
+    document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+   } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+}
 }
